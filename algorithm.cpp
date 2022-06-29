f12d8cf1838d13945f2a36e6ce463fe5af9c7076
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-06-29 17:21:13 +0500

    Update algorithm.cpp

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,51 +1,55 @@
-class Solution
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <queue>
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq2;
+vector<int> res;
+int count;
+void insertArrivedProcess(vector<vector<int>> &tasks)
 {
-    priority_queue<pair<int, int>, vector< pair<int, int>>, greater<pair<int, int>>> pq;
-    priority_queue<pair<int, int>, vector< pair<int, int>>, greater<pair<int, int>>> pq2;
-    vector<int> res;
-    unsigned long long int count;
-    void insertArrivedProcess(vector<vector < int>> &tasks)
+    while (!pq2.empty() and pq2.top().first <= count)
     {
-        while (!pq2.empty() and pq2.top().first <= count)
+        pq.push({tasks[pq2.top().second][1], pq2.top().second});
+        pq2.pop();
+    }
+}
+vector<int> getOrder(vector<vector<int>> &tasks)
+{
+
+    for (int i = 0; i < tasks.size(); i++)
+    {
+        pq2.push({tasks[i][0], i});
+    }
+    cout << pq2.top().first;
+    count = pq2.top().first;
+    while (!pq2.empty() and pq2.top().first <= count)
+    {
+        pq.push({tasks[pq2.top().second][1], pq2.top().second});
+        pq2.pop();
+    }
+    int val;
+    while (!pq.empty())
+    {
+        res.push_back(pq.top().second);
+        val = pq.top().second;
+        count += pq.top().first;
+        pq.pop();
+        if (!pq2.empty())
         {
-            pq.push({ tasks[pq2.top().second][1],
-                pq2.top().second });
-            pq2.pop();
+            while (pq2.top().first < count and pq2.top().first <= coun)
+                count++;
+            insertArrivedProcess(tasks);
         }
     }
 
-    public:
-        vector<int> getOrder(vector<vector < int>> &tasks)
-        {
-           	
+    return res;
+}
+int main()
+{
+    vector<vector<int>> heights = {{5, 2}, {7, 2}, {9, 4}, {6, 3}, {5, 10}, {1, 1}};
 
-            for (int i = 0; i < tasks.size(); i++)
-            {
-                pq2.push({ tasks[i][0],
-                    i });
-            }
-            cout << pq2.top().first;
-            count = pq2.top().first;
-            while (!pq2.empty() and pq2.top().first <= count)
-            {
-                pq.push({ tasks[pq2.top().second][1],
-                    pq2.top().second });
-                pq2.pop();
-            }
-
-            while (!pq.empty())
-            {
-                res.push_back(pq.top().second);
-                count += pq.top().first;
-                pq.pop();
-                if (!pq2.empty())
-                {
-                    while (pq2.top().first > count and pq.empty())
-                        count++;
-                    insertArrivedProcess(tasks);
-                }
-            }
-
-            return res;
-        }
-};
+    getOrder(heights);
+}
